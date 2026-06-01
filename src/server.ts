e72e97c1e3aac98954f8085bb142af2a834b465d
Luísa Vernersbach Varejão <luisavvarejao@hotmail.com>
2026-06-01 20:53:34 -0300

    Delete src directory

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-import { createApp } from './app';
-
-const app = createApp();
-const port = Number(process.env.PORT ?? 3333);
-
-app.listen(port, () => {
-  console.log(`🚀 API rodando em http://localhost:${port}`);
-});
